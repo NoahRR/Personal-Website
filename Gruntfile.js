d27bd3e6adfd5ce87312b232416b279b6e7dfc36
Noah Ray Roberts <noahray216@gmail.com>
2020-12-16 09:05:04 -0600

    Concat all css files. Works with grunt watch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,23 @@
 
 module.exports = function (grunt) {
     grunt.initConfig({
+        concat: {
+            css: {
+                src: [
+                    'app/static/css/portfolio.css',
+                    'app/static/css/featured.css',
+                    'app/static/css/header.css',
+                    'app/static/css/footer.css'
+                ],
+                dest: 'app/static/css/CONCAT_CSS.css'
+            }
+        },
+        watch: {
+            css: {
+                files: ['**/*.css'],
+                tasks: ['concat']
+            }
+        },
         browserSync: {
             bsFiles: {
                 src: 'app'
@@ -11,6 +28,12 @@ module.exports = function (grunt) {
         }
     });
 
+    // grunt browserSync
     grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.registerTask('default', ['browserSync']);
+    // grunt concat
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt watch
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt
+    grunt.registerTask('default', ['concat', 'watch']);
 }
